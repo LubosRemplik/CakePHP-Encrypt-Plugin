96b46c3f0152d348b6b03a6821fa71690a972890
Lubos Remplik <lubos@on-idle.com>
2011-05-18 09:03:17 +0100

    Added pgp component which encrypt email vi GnuPG

---
+++ controllers/components/pgp.php
@@ -0,0 +1,38 @@
+<?php
+
+class PgpComponent extends Object {
+	
+	function format($text) {
+		$text = str_replace("'", "", $text);
+		$text = str_replace("(", "-", $text);
+		$text = str_replace(")", "-", $text);
+		return $text;
+	}
+	
+	function encrypt($home = "/home/lubos/.gnupg", $recipient, $message) {
+		$debugThis = Configure::read('PGPMail.debug');
+		
+		$home = escapeshellarg($home);
+		$recipient = escapeshellarg($recipient);
+		$message = escapeshellarg($message);
+		
+		$command = "/home/lubos/encrypt.sh " . $home . " " . $recipient . " " . $message . "";
+		
+		if ($debugThis) debug($command);
+		
+		$returnValue = exec($command, $ret, $err);
+		
+		if ($debugThis) debug($returnValue);
+		if ($debugThis) debug($ret);
+		if ($debugThis) debug($err);
+		
+		$messageEncrypted = "";
+		for ($i=0; $i < count($ret); $i++) {
+			$messageEncrypted = $messageEncrypted.$ret[$i] . "\n";
+		}
+		
+		if ($debugThis) debug($messageEncrypted);
+		
+		return $messageEncrypted;
+	}
+}
