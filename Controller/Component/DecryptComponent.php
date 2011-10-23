9db538963ff8c3438a85cfdcd62d16a2e572fdfc
Lubos Remplik <lubos@lubos.me>
2011-10-23 08:05:39 -0700

    Decrypt component layout fix

--- Controller/Component/DecryptComponent.php
+++ Controller/Component/DecryptComponent.php
@@ -1,6 +1,41 @@
 <?php
-
+/**
+ * Decrypt Component
+ **/
 class DecryptComponent extends Object {
+    
+    public $components = array();
+
+    //called before Controller::beforeFilter()
+    public function initialize(&$controller, $settings = array()) {
+        // saving the controller reference for later use
+        $this->controller =& $controller;
+    }
+
+    //called after Controller::beforeFilter()
+    public function startup(&$controller) {
+    
+    }
+
+    //called after Controller::beforeRender()
+    public function beforeRender(&$controller) {
+    
+    }
+
+    //called after Controller::render()
+    public function shutdown(&$controller) {
+    
+    }
+
+    //called before Controller::redirect()
+    public function beforeRedirect(&$controller, $url, $status=null, $exit=true) {
+    
+    }
+
+    public function redirectSomewhere($value) {
+        // utilizing a controller method
+        $this->controller->redirect($value);
+    }
 	
 	public function hex2bin($h) {
 		if(!is_string($h)) return null;
@@ -11,4 +46,3 @@ class DecryptComponent extends Object {
 		return $r;
 	}
 }
-
